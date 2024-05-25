7a1f193284546fdfdfb2198a0c7d5077bae03642
TITAN <aluochmarion7@gmail.com>
2024-05-25 23:46:51 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,word):
+        self.word=word
+    
+    def match(self,word_list):
+        sorted_word=sorted(self.word)
+        matching=[]
+        
+        for word in word_list:
+            if sorted_word==sorted(word):
+                matching.append(word)
+            
+        return matching
+    
